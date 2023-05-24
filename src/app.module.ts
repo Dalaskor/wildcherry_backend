ad9fbe969d44f81d3e0e20d38b8acbd0c33593b4
Danila Skripko <dalas.prog@gmail.com>
2023-05-24 11:18:29 +0400

    Update config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { SequelizeModule } from '@nestjs/sequelize';
 import * as Joi from 'joi';
 
@@ -10,15 +11,27 @@ import * as Joi from 'joi';
       validationSchema: Joi.object({
         PORT: Joi.number().required(),
         POSTGRES_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_REFRESH_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
       }),
       envFilePath: './.env',
     }),
     SequelizeModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
-          uri: configService.get<string>('POSTGRES_URI'),
-          dialect: 'postgres',
-          models: [],
-          autoLoadModels: true,
+        uri: configService.get<string>('POSTGRES_URI'),
+        dialect: 'postgres',
+        models: [],
+        autoLoadModels: true,
+      }),
+      inject: [ConfigService],
+    }),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION')}s`,
+        },
       }),
       inject: [ConfigService],
     }),
