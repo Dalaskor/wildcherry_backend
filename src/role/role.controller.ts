05e136b61263dfab920855904e230a2f7f3fb73c
Danila Skripko <dalas.prog@gmail.com>
2023-05-26 12:44:10 +0400

    Update roles

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,6 +1,7 @@
+import { ROLES, Roles, RolesGuard } from '@app/common';
 import { CreateRoleDto, Role, UpdateRoleDto } from '@app/database';
 import {
-    BadRequestException,
+  BadRequestException,
   Body,
   Controller,
   Delete,
@@ -9,8 +10,10 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import {
+  ApiBearerAuth,
   ApiBody,
   ApiOperation,
   ApiParam,
@@ -23,7 +26,10 @@ import { RoleSerivce } from './role.service';
 @Controller('role')
 export class RoleController {
   constructor(private roleService: RoleSerivce) {}
-  @ApiOperation({ summary: 'Создание новой роли' })
+  @ApiOperation({
+    summary: 'Создание новой роли',
+    description: 'Требуется роль ADMIN',
+  })
   @ApiBody({
     type: CreateRoleDto,
     description: 'DTO для создания роли',
@@ -33,17 +39,26 @@ export class RoleController {
     status: HttpStatus.BAD_REQUEST,
     description: 'Ошибка при создании роли',
   })
+  @ApiBearerAuth()
+  @Roles(ROLES.ADMIN)
+  @UseGuards(RolesGuard)
   @Post()
   async create(@Body() dto: CreateRoleDto): Promise<Role> {
     return this.roleService.create(dto);
   }
   @ApiOperation({ summary: 'Получить все роли' })
   @ApiResponse({ status: HttpStatus.OK, type: Role, isArray: true })
+  @ApiBearerAuth()
+  @Roles(ROLES.ADMIN)
+  @UseGuards(RolesGuard)
   @Get()
   async getAll(): Promise<Role[]> {
     return this.roleService.getAll();
   }
-  @ApiOperation({ summary: 'Получить одну роль по id' })
+  @ApiOperation({
+    summary: 'Получить одну роль по id',
+    description: 'Требуются роль ADMIN',
+  })
   @ApiParam({
     name: 'id',
     type: Number,
@@ -59,7 +74,10 @@ export class RoleController {
     }
     return this.roleService.getOne(id);
   }
-  @ApiOperation({ summary: 'Обновить роль по id' })
+  @ApiOperation({
+    summary: 'Обновить роль по id',
+    description: 'Требуется роль ADMIN',
+  })
   @ApiParam({
     name: 'id',
     type: Number,
@@ -72,6 +90,9 @@ export class RoleController {
   })
   @ApiResponse({ status: HttpStatus.OK, type: Role })
   @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Роль не найдена' })
+  @ApiBearerAuth()
+  @Roles(ROLES.ADMIN)
+  @UseGuards(RolesGuard)
   @Put('/:id')
   async update(
     @Param('id') id: number,
@@ -82,7 +103,7 @@ export class RoleController {
     }
     return this.roleService.update(id, dto);
   }
-  @ApiOperation({ summary: 'Удалить роль по id' })
+  @ApiOperation({ summary: 'Удалить роль по id', description: 'Требуется роль ADMIN' })
   @ApiParam({
     name: 'id',
     type: Number,
@@ -91,6 +112,9 @@ export class RoleController {
   })
   @ApiResponse({ status: HttpStatus.OK, type: Role })
   @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Роль не найдена' })
+  @ApiBearerAuth()
+  @Roles(ROLES.ADMIN)
+  @UseGuards(RolesGuard)
   @Delete('/:id')
   async delete(@Param('id') id: number): Promise<Role> {
     if (!Number(id)) {
